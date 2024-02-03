b9f7d7d75feee9a363061abd153fdd80ccf4040e
dynamic soni <154851547+swarnkar1992@users.noreply.github.com>
2024-02-03 16:48:47 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -67,7 +67,7 @@ async def _batch(event):
             try:
                 value = int(_range.text)
                 if value > 1000:
-                    await conv.send_message("You can only get upto 1000 files in a single batch.")
+                    await conv.send_message("You can only get upto 100 files in a single batch.")
                     return conv.cancel()
             except ValueError:
                 await conv.send_message("Range must be an integer!")
